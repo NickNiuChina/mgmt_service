f25986c12b56f85481d7c7c80fbd1d7c6074ea57
nickniu1 <448562031@qq.com>
2022-08-26 13:17:17 +0800

    fix some bugs

--- lib/Mgmt/Service.pm
+++ lib/Mgmt/Service.pm
@@ -4,46 +4,50 @@ use Mojo::Base 'Mojolicious', -signatures;
 # This method will run once at server start
 sub startup ($self) {
 
-  # Load configuration from config file
-  my $config = $self->plugin('NotYAMLConfig');
+    # Load configuration from config file
+    my $config = $self->plugin('NotYAMLConfig');
 
-  # Configure the application
-  $self->secrets($config->{secrets});
+    # Configure the application
+    $self->secrets($config->{secrets});
 
-  # Router
-  my $r = $self->routes;
+    # Router
+    my $r = $self->routes;
+    
+    $r->get('/')->to('Base#index');
 
-  # Normal route to controller
-  # $r->get('/')->to('Example#welcome');
-  # $r->get('/upload_image')->to(controller => 'UploadImageController', action => 'index');
+    # Normal and secured routes to controller
+    # $r->get('/')->to('Example#welcome');
+    # $r->get('/upload_image')->to(controller => 'UploadImageController', action => 'index');
+    $r->get('/service')->to('Base#login');
+    my $auth = $r->under('/service')->to('Base#authCheck');
+    $r->post('/service/login')->to('Base#loginValidate');
 
-  $r->get('/service')->to('Login#index');
-  $r->get('/')->to('Base#index');
-  $r->get('/service/tips')->to('Base#index');
-  
-  $r->get('/service/clientstatus')->to('Views#clientsStatus');
-  $r->post('/service/clientstatus/list')->to('Views#clientsStatuslist');
 
-  $r->get('/service/issue')->to('Views#issuecert');
-  $r->post('/service/issue/upload')->to('Views#reqUpload');
+    $auth->get('/tips')->to('Base#showHelp');
+    
+    $auth->get('/clientstatus')->to('Views#clientsStatus');
+    $auth->post('/clientstatus/list')->to('Views#clientsStatuslist');
 
-  $r->get('/service/reqs')->to('Views#reqFilesList');
-  $r->post('/service/reqs/list')->to('Views#reqsClientsListJson');
-  $r->post('/service/reqs/delete')->to('Views#reqClientsDelete');
-  # They can be especially useful for manually matching file names with extensions, rather than using format detection.
-  # /music/song.mp3 -> /music/#filename -> {filename => 'song.mp3'}
-  $r->get('/service/reqs/dl/#filename')->to('Views#reqClientsDownload');
-  
-  $r->get('/service/certed')->to('Views#certedClientsList');
-  $r->post('/service/certed/list')->to('Views#certedClientsListJson');
-  $r->post('/service/certed/delete')->to('Views#certedClientsDelete');
-  $r->post('/service/certed/download')->to('Views#certedClientsDownload');
-  $r->get('/service/certed/dl/#filename')->to('Views#certedClientsDownload');
-  
-  
-  ###### Other Urls ########
-  # $r->get('/bar')->to('Foo#bar');
-  $r->get('/bar')->to(controller => 'Foo', action => 'bar');
+    $auth->get('/issue')->to('Views#issuecert');
+    $auth->post('/issue/upload')->to('Views#reqUpload');
+
+    $auth->get('/reqs')->to('Views#reqFilesList');
+    $auth->post('/reqs/list')->to('Views#reqsClientsListJson');
+    $auth->post('/reqs/delete')->to('Views#reqClientsDelete');
+    # They can be especially useful for manually matching file names with extensions, rather than using format detection.
+    # /music/song.mp3 -> /music/#filename -> {filename => 'song.mp3'}
+    $auth->get('/reqs/dl/#filename')->to('Views#reqClientsDownload');
+    
+    $auth->get('/certed')->to('Views#certedClientsList');
+    $auth->post('/certed/list')->to('Views#certedClientsListJson');
+    $auth->post('/certed/delete')->to('Views#certedClientsDelete');
+    $auth->post('/certed/download')->to('Views#certedClientsDownload');
+    $auth->get('/certed/dl/#filename')->to('Views#certedClientsDownload');
+    
+    
+    ###### Other Urls ########
+    # $r->get('/bar')->to('Foo#bar');
+    $r->get('/bar')->to(controller => 'Foo', action => 'bar');
 
 }
 
--- lib/Mgmt/Service/Controller/Base.pm
+++ lib/Mgmt/Service/Controller/Base.pm
@@ -3,10 +3,87 @@ use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 # This action will render a template
 sub index ($self) {
-
   # Render template "somedir/fn.html.ep" with message
   $self = shift;
-  return $self->redirect_to('/service');
+  return $self->redirect_to('service');
+}
+
+sub login ($c) {
+  
+  $c->stash( error   => $c->flash('error') );
+  $c->stash( message => $c->flash('message') );
+  $c->render(template => 'base/login');
+}
+
+sub loginValidate ($c) {
+
+    # List of registered users
+    my (undef,undef,undef,$mday,$mon,undef) = localtime;
+    $mon = $mon + 1;
+    my $len = length($mon);
+    if ($len < 2){
+      $mon = '0' . $mon;
+    }
+    my $tempPass = 'nimda' . "$mon$mday";
+    my %validUsers = ( "admin" => $tempPass );
+    # Get the user name and password from the page
+    my $user = $c->param('username');
+    my $password = $c->param('password');
+    # debug info
+    print "password: $password\n";
+    print "tempPass: $tempPass\n";
+    if ($password eq $tempPass) {
+      print "Password Match\n";
+    }
+    else {
+      print ("Password Not Match\n");
+    }
+    # First check if the user exists
+    if($validUsers{$user}){
+        # Validating the password of the registered user
+        if($validUsers{$user} eq $password){
+            # Creating session cookies
+            $c->session(is_auth => 1);             # set the logged_in flag
+            $c->session(username => $user);        # keep a copy of the username
+            $c->session(expiration => 1800);        # expire this session in 10 minutes if no activity
+            # Re-direct to home page
+            # &welcome($c);
+            $c->redirect_to('/service/clientstatus')
+        }else{
+            # If password is incorrect, re-direct to login page and then display appropriate message
+            $c->flash( error => 'Invalid User/Password, please try again' );
+            return $c->redirect_to("/service");
+        }
+    } else {
+        # If user does not exist, re-direct to login page and then display appropriate message
+        $c->flash( error => 'Invalid Username or Password, please try again' );
+        return $c->redirect_to("/service");
+    }
+    
+    sub authCheck {
+      my $c = shift;
+      # checks if session flag (is_auth) is already set
+      return 1 if $c->session('is_auth');
+      # If session flag not set re-direct to login page again.
+      # $self->redirect_to(template => "myTemplates/login", error_message =>  "You are not logged in, please login to access this website");
+      # return;
+      # $c->stash( error   => $c->flash('error') );
+      # $c->stash( message => $c->flash('message') );
+      # $c->render(template => 'base/login');
+      # return;
+      $c->redirect_to('/service');
+      return undef;  
+    }
+
+    sub logout ($c) {
+      # Remove session and direct to logout page
+      $c->session(expires => 1);  #Kill the Session
+      return $c->redirect_to("/service");
+    }
+
+    sub showHelp ($c) {
+        $c->render(template => 'base/base');
+    }
 }
 
 1;
