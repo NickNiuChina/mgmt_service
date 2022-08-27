8c7de38d62fcf200f2b35dc321058dd30169a916
nickniu1 <448562031@qq.com>
2022-08-27 11:43:45 +0800

    Update test url

--- lib/Mgmt/Service/Controller/Test.pm
+++ lib/Mgmt/Service/Controller/Test.pm
@@ -5,12 +5,20 @@ use Mojo::Home;
 
 # Action
 sub test ($c) {
-  my $name = $c->req->param('name');
+  # get params name
+  my $name = $c->req->param('name'); 
   my $home = Mojo::Home->new;
+  opendir(DIR, $home) or die "can't open $home: $!";
+  my @files = readdir DIR;
+  closedir DIR;
+
   $c->res->headers->cache_control('max-age=1, no-cache');
   $c->render(
-    json => {hello => $name, 
-    MojoHome => $home }
+    json => {
+      hello => $name, 
+      MojoHome => $home,
+      File => @files 
+      }
   );
 }
 
